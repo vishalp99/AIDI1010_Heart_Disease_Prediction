824932aebe1a5f4be0cfef579fa0d0caea7fe6f8
vishalp99 <vishalnpatel999@gmail.com>
2025-11-30 01:34:19 -0500

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 # --- Load TPOT model ---
 with open('files/tpot_model.pkl', 'rb') as f:
@@ -75,4 +76,5 @@ def predict():
 
 # --- Run the app ---
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
